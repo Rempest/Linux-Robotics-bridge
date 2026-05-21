b6c1277effdbe47857544dd6b2f2198f03e66c19
Raempest <thehuman130410@gmail.com>
2026-05-21 18:33:57 +0500

    Add installation script for ROS2 Humble
    This script installs ROS2 Humble, including necessary dependencies and setting up the environment.

--- package/ros2.sh
+++ package/ros2.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+install_ros2() {
+    echo "[INFO] Installing ROS2 Humble..."
+
+    sudo apt update
+
+    sudo apt install -y \
+        software-properties-common \
+        curl
+
+    sudo add-apt-repository universe -y
+
+    sudo curl -sSL \
+      https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
+      -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) \
+      signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
+      http://packages.ros.org/ros2/ubuntu \
+      $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | \
+      sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+    sudo apt update
+
+    sudo apt install -y \
+        ros-humble-desktop \
+        python3-colcon-common-extensions
+
+    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+
+    mkdir -p ~/ros2_ws/src
+
+    echo "[SUCCESS] ROS2 installed."
+}
