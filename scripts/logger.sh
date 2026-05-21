cea041868c7f5ac131631b8f2820e03eb643766d
Raempest <thehuman130410@gmail.com>
2026-05-21 19:02:27 +0500

    Update logger.sh

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+source scripts/colors.sh
+
+log_info() {
+    echo -e "${BLUE}[INFO]${NC} $1"
+}
+
+log_success() {
+    echo -e "${GREEN}[SUCCESS]${NC} $1"
+}
+
+log_warning() {
+    echo -e "${YELLOW}[WARNING]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
