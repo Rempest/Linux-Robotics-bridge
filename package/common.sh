af9cd6634b88787ea44336367333e31dfbf32d11
Raempest <thehuman130410@gmail.com>
2026-05-21 18:39:26 +0500

    Fix shebang and improve install message

--- package/common.sh
+++ package/common.sh
@@ -1,19 +1,20 @@
-#!bin/bash
+#!/bin/bash
 
 install_common_packages() {
-    echo "[INFO] Installing common package..."
+    echo "[INFO] Installing common packages..."
 
     sudo apt update
 
     sudo apt install -y \
-         curl \
-         wget \
-         git \
-         vim \
-         htop \
-         tmux \
-         net-tools \
-         build-essential \
-         software-properties-common
-    echo"[SUCCES] Common packages installed."  
+        curl \
+        wget \
+        git \
+        vim \
+        htop \
+        tmux \
+        net-tools \
+        build-essential \
+        software-properties-common
+
+    echo "[SUCCESS] Common packages installed."  
 }
