e883c367acfe1dfa4ce43264b9b691986727ec1d
Raempest <thehuman130410@gmail.com>
2026-05-21 18:41:08 +0500

    Update VSCode installation script
    Fixed file name and corrected package manager command.

--- package/vscode.sh
+++ package/vscode.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+
 install_vscode() {
     echo "[INFO] Installing VSCode..."
+    
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
          gpg --dearmor > packages.microsoft.gpg
+         
     sudo install -D -o root -g root -m 644 \
          packages.microsoft.gpg \
          /etc/apt/keyrings/packages.microsoft.gpg
+         
     echo \
       "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] \
       https://packages.microsoft.com/repos/code stable main" | \
-      sudo tee /etc/apt/sources.list.d/vscode.list
-    rm -f package.microsoft.gpg
+      sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+      
+    rm -f packages.microsoft.gpg # Исправлено имя файла
 
     sudo apt update
-
-    sudo apr install -y code
+    sudo apt install -y code # Исправлено: apt вместо apr
 
     echo "[SUCCESS] VSCode installed."
-  
 }
