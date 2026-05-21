20e7030e1244e0fe595ea62e228160d59bf2fd3b
Raempest <thehuman130410@gmail.com>
2026-05-21 18:16:32 +0500

    Add function to install common packages

--- package/common.sh
+++ package/common.sh
@@ -1,1 +1,19 @@
+#!bin/bash
 
+install_common_packages() {
+    echo "[INFO] Installing common package..."
+
+    sudo apt update
+
+    sudo apt install -y \
+         curl \
+         wget \
+         git \
+         vim \
+         htop \
+         tmux \
+         net-tools \
+         build-essential \
+         software-properties-common
+    echo"[SUCCES] Common packages installed."  
+}
