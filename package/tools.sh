3fdcabae009ef2ef54465e3469d47d6a1f5bdbb0
Raempest <thehuman130410@gmail.com>
2026-05-21 18:35:32 +0500

    Add script to install developer tools

--- package/tools.sh
+++ package/tools.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+install_tools(){
+    echo "[INFO] Installing developer tools..."
+
+    sudo apt install -y \
+     neofetch \
+        tree \
+        unzip \
+        zip \
+        btop \
+        cmake
+    echo "[SUCCESS] Developer tools installed."
+}
